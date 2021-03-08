3f5cc6f7aaa63c1b956278f2b378330ff7a35d3d
Nekhala <Nekhalahuss@gmail.com>
2021-03-08 09:28:15 +0100

    my first commin

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <Modal :header="header" :text="text" theme="sale"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: 'My first vue app :)',
+      header: 'Sign up for giveaway',
+      text: 'Grab your ninja swag for half price!'
+    }
   }
 }
 </script>
@@ -22,5 +27,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale h1 {
+  color: white;
 }
 </style>
